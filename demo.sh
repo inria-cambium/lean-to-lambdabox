b5eb8107b4daa8158fa1bed3fe9157f980238d5d
Simon Dima <simon.dima@gmail.com>
2025-05-22 13:20:31 +0200

    Removed Coq pretty-printing demo

--- demo.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/zsh
-lake exe lean_to_lambdabox | tee >(coqc -Q theories LeanToLambdaBox -w "-extraction-reserved-identifier" theories/deserialize_and_pp.v)
--- theories/test_erasure.v
+++
@@ -1,8 +0,0 @@
-Require Import MetaCoq.ErasurePlugin.Loader.
-
-Fixpoint
-  even n := match n with O => true | S n => odd n end
-with
-  odd n := match n with O => false | S n => even n end
-.
-MetaCoq Erase even.
