600075548ac783a95ee704631b1b8d08f4fbcb59
Simon Dima <simon.dima@gmail.com>
2025-07-23 15:48:00 +0200

    Clean up FromLeanCommon

--- benchmarks/from_lean/FromLeanCommon.lean
+++ benchmarks/from_lean/FromLeanCommon.lean
@@ -8,25 +8,22 @@ import FromLeanCommon.rbmap
 import FromLeanCommon.rbmap_std
 import FromLeanCommon.unionfind
 
-set_option linter.unusedVariables false
+def unit (_: Unit): Unit := .unit
 
-def unit (u: Unit): Unit := .unit
+def demo0 (_: Unit): List Unit := [.unit, .unit, .unit]
 
-def demo0 (u: Unit): List Unit := [.unit, .unit, .unit]
+def demo1 (_: Unit): List Bool := List.replicate 5000 true |>.append <| List.replicate 3000 false
 
--- TODO: also test version using typeclass search to generate HAppend instance from "++" notation
-def demo1 (u: Unit): List Bool := List.replicate 5000 true |>.append <| List.replicate 3000 false
-
-def demo1_tc (u: Unit): List Bool := List.replicate 5000 true ++ List.replicate 3000 false
+def demo1_tc (_: Unit): List Bool := List.replicate 5000 true ++ List.replicate 3000 false
 
 def repeat2 (x y: α) (n: Nat): List α :=
   match n with
   | 0 => []
   | n+1 => x :: y :: repeat2 x y n
 
-def demo2 (u: Unit) := List.map not (repeat2 true false 100)
+def demo2 (_: Unit) := List.map not (repeat2 true false 100)
 
-def demo3 (u: Unit) := and
+def demo3 (_: Unit) := and
 
 -- List.sum has a bug which causes the csimp List.foldr -> List.foldrTR not to apply, which leads to stack overflows.
 -- https://github.com/leanprover/lean4/issues/7750
@@ -51,7 +48,7 @@ def shared_list_sum_rev n :=
   let res := l.reverse.foldl Nat.add 0
   res + l[0]!
 
-def cube (u: Unit) := 300^3
+def cube (_: Unit) := 300^3
 
 def triangle (n: Nat) := List.range n |>.foldl Nat.add 0
 
@@ -70,14 +67,6 @@ def sub_3 (n: Nat): Nat :=
   n-3
 
 def iflazy (n: Nat): Nat := if n = 0 then 42 else iflazy (n-1)
-set_option pp.notation false
-set_option pp.explicit true
-/-
-#print iflazy._unsafe_rec
-#print ite
-#print instDecidableEqNat
-#print Nat.decEq
--/
 
 mutual
 def even: Nat -> Nat
